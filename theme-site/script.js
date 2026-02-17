3e9e81fcb8a7d4173fd6054c3b23b592d0789a97
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-02-17 19:53:51 +0530

    Update script.js

--- theme-site/script.js
+++ theme-site/script.js
@@ -1,5 +1,15 @@
 const btn = document.getElementById("toggle");
 
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
 btn.onclick = () => {
   document.body.classList.toggle("dark");
+
+  const theme = document.body.classList.contains("dark")
+    ? "dark"
+    : "light";
+
+  localStorage.setItem("theme", theme);
 };
