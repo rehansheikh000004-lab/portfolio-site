f6fa30ac39d57ef3641fe39ffe30b2acecef9a4d
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-02-18 11:28:51 +0530

    Update script.js

--- responsive-site/script.js
+++ responsive-site/script.js
@@ -1,7 +1,21 @@
 const btn = document.getElementById("menuBtn");
 const nav = document.getElementById("navLinks");
 
+/* MENU */
 btn.onclick = () => {
   nav.style.display =
     nav.style.display === "flex" ? "none" : "flex";
 };
+
+/* SCROLL REVEAL */
+const reveals = document.querySelectorAll(".reveal, .reveal-strong");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 80) {
+      el.classList.add("active");
+    }
+  });
+});
